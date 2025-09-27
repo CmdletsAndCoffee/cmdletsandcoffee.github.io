2278832566d99adfac401703e24c9de12a619bfe
Tommy Becker <tommy.becker@gmail.com>
2025-09-27 12:29:29 -0500

    fix: correct types in getPosts utility

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -1,10 +1,10 @@
-import { getCollection, type CollectionEntry } from 'astro:content';
+import { getCollection } from 'astro:content';
 
-export async function getPosts(collections: string[] = ['blog']): Promise<any[]> {
+export async function getPosts(collections: readonly string[] = ['blog']): Promise<any[]> {
   const allPosts = await Promise.all(
     collections.map(async (collection) => {
       const posts = await getCollection(collection as any);
-      const filteredPosts = posts.filter(post => import.meta.env.PROD ? !post.data.draft : true);
+      const filteredPosts = posts.filter((post: any) => import.meta.env.PROD ? !post.data.draft : true);
       return filteredPosts;
     })
   );
